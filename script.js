a98b18cb6fc03f91fca0ea7c7dcaeed62585d3da
konradadev <konradadev@gmail.com>
2022-03-10 18:41:48 +0100

    Add functions for basic operations

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //JS
+const add = (addendA, addendB) => addendA+addendB;
+  
+const subtract = (minuend, subtrahend) => minuend-subtrahend;
+
+const multiply = (factorA, factorB) => factorA*factorB;
+
+const divide = (dividend, divisor) => dividend/divisor;
+
