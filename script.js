a072f9dbe659ada519405bb0d63c33d435c14792
konradadev <konradadev@gmail.com>
2022-03-10 18:50:16 +0100

    Add function to call operations

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ const multiply = (factorA, factorB) => factorA*factorB;
 
 const divide = (dividend, divisor) => dividend/divisor;
 
+function operate(operator, a, b){
+    let result = null;
+    switch(operator){
+        case "+":
+            result = add(a,b);
+            break;
+        case "-":
+            result = subtract(a,b);
+            break;
+        case "*":
+            result = multiply(a,b);
+            break;
+        case "/":
+            result = divide(a,b);
+            break;
+        default:
+            return "Error! Bad operator";
+    }
+    return result;
+}
